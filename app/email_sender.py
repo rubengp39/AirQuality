bdc0a9a4706b1d3bc1683630e7eb874d29288366
Rubén García Pedrejón <62309228+rubengp39@users.noreply.github.com>
2022-12-19 20:15:20 +0100

    Upload source files from raspi

---
+++ app/email_sender.py
@@ -0,0 +1,55 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email import encoders
+import os
+
+def sendEmail(smtpHost, smtpPort, mailUname, mailPwd, fromEmail, mailSubject, mailContentHtml, recepientsMailList, attachmentFpaths):
+    # create message object
+    msg = MIMEMultipart()
+    msg['From'] = fromEmail
+    msg['To'] = ','.join(recepientsMailList)
+    msg['Subject'] = mailSubject
+    # msg.attach(MIMEText(mailContentText, 'plain'))
+    msg.attach(MIMEText(mailContentHtml, 'html'))
+
+    # create file attachments
+    for aPath in attachmentFpaths:
+        # check if file exists
+        part = MIMEBase('application', "octet-stream")
+        part.set_payload(open(aPath, "rb").read())
+        encoders.encode_base64(part)
+        part.add_header('Content-Disposition',
+                        'attachment; filename="{0}"'.format(os.path.basename(aPath)))
+        msg.attach(part)
+
+    # Send message object as email using smptplib
+    s = smtplib.SMTP(smtpHost, smtpPort)
+    s.starttls()
+    s.login(mailUname, mailPwd)
+    msgText = msg.as_string()
+    sendErrs = s.sendmail(fromEmail, recepientsMailList, msgText)
+    s.quit()
+
+    # check if errors occured and handle them accordingly
+    if not len(sendErrs.keys()) == 0:
+        raise Exception("Errors occurred while sending email", sendErrs)
+
+
+# mail server parameters
+smtpHost = "smtp.gmail.com"
+smtpPort = 587
+mailUname = 'senderemail@gmail.com'
+mailPwd = 'ehqpsvygwbmujclq'
+fromEmail = 'senderemail@gmail.com'
+
+# mail body, recepients, attachment files
+mailSubject = "test subject"
+mailContentHtml = "Hi, Hope u are fine. <br/> This is a <b>test</b> mail from python script using an awesome library called <b>smtplib</b>"
+recepientsMailList = ["receiveremail@gmail.com"]
+attachmentFpaths = ["smtp.png", "poster.png"]
+sendEmail(smtpHost, smtpPort, mailUname, mailPwd, fromEmail,
+          mailSubject, mailContentHtml, recepientsMailList, attachmentFpaths)
+
+print("execution complete...")
---
+++ app/main.py
@@ -0,0 +1,71 @@
+import json
+import asyncio
+from azure.iot.device.aio import IoTHubDeviceClient
+import sys
+from device_provisioning_service import Device
+from sensor import Sensor
+import os
+
+import ptvsd
+ptvsd.enable_attach(address=('0.0.0.0', 5678))
+# ptvsd.wait_for_attach()
+
+
+async def main():
+    scopeID = None
+    deviceId = None
+    key = None
+
+    scopeID = os.getenv('SCOPE_ID')
+    deviceId = os.getenv('DEVICE_ID')
+    key = os.getenv('DEVICE_KEY')
+
+    if scopeID is None or deviceId is None or key is None:
+        sys.exit(1)
+
+    dps = Device(scopeID, deviceId, key)
+
+    conn_str = await dps.connection_string
+
+    device_client = IoTHubDeviceClient.create_from_connection_string(conn_str)
+    await device_client.connect()
+
+    sensor = Sensor()
+
+    async def message_listener(device_client):
+        while True:
+            message = await device_client.receive_message()  # blocking call
+            print("the data in the message received was ")
+            print(message.data)
+            print("custom properties are")
+            print(message.custom_properties)
+
+    asyncio.create_task(message_listener(device_client))
+
+    while True:
+        try:
+            telemetry = await sensor.readSensor()
+            if telemetry is not None:
+
+                print(telemetry)
+                data = json.dumps(telemetry)
+                if data['pm1'] > 10 | data['pm25'] > 10 | data['pm10'] > 10:
+                    pass
+
+                await device_client.send_message(data)
+
+                await asyncio.sleep(60)
+
+        except:
+            print("Unexpected error:", sys.exc_info()[0])
+
+    await device_client.disconnect()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
+    # If using Python 3.6 or below, use the following code instead of asyncio.run(main()):
+    # loop = asyncio.get_event_loop()
+    # loop.run_until_complete(main())
+    # loop.close()
